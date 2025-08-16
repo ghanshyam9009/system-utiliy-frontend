d8637483822257b7a6cce8ae387e76ba58ebf7ad
ghanshyam <ghanshyamchoudhary9009>
2025-08-16 15:08:50 +0530

    Update API_BASE URL to include '/api' endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE = "http://localhost:5000/api";
-const API_BASE = "https://system-utility-backend.onrender.com";
+const API_BASE = "https://system-utility-backend.onrender.com/api";
 
 export const fetchAllMachines = async () => {
   const res = await axios.get(`${API_BASE}/all-machines`);
