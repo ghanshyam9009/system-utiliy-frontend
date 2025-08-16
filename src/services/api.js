336214ab1a6d039f3bf50cd597b8f89c871e7c3c
ghanshyam <ghanshyamchoudhary9009>
2025-08-16 14:43:44 +0530

    Update API_BASE URL for backend service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE = "http://localhost:5000/api";
-const API_BASE = "https://system-utiliy-frontend.onrender.com/";
+const API_BASE = "https://system-utility-backend.onrender.com/";
 
 export const fetchAllMachines = async () => {
   const res = await axios.get(`${API_BASE}/all-machines`);
